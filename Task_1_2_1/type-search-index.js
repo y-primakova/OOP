efb11f9a5b6c19a056e989b90d2ab8cb3184013b
y-primakova <y-primakova@users.noreply.github.com>
2023-10-25 04:39:32 +0000

    Deploying to gh-pages from @ y-primakova/OOP@f5885590ee4126114117a14582205cba29990364 🚀

--- Task_1_2_1/type-search-index.js
+++ Task_1_2_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.primakova","l":"IteratorBfs"},{"p":"ru.nsu.primakova","l":"IteratorDfs"},{"p":"ru.nsu.primakova","l":"Main"},{"p":"ru.nsu.primakova","l":"Tree"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.primakova","l":"IteratorBfs"},{"p":"ru.nsu.primakova","l":"IteratorDfs"},{"p":"ru.nsu.primakova","l":"Main"},{"p":"ru.nsu.primakova","l":"NullNodeException"},{"p":"ru.nsu.primakova","l":"Tree"}]
