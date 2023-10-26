7de627bbaefe8769d418db4a73cb06a49ea767ab
Yulia Primakova <y.primakova@g.nsu.ru>
2023-10-26 14:56:04 +0700

    Test_1_2_1

--- Task_1_2_1/src/main/java/ru/nsu/primakova/IteratorDfs.java
+++ Task_1_2_1/src/main/java/ru/nsu/primakova/IteratorDfs.java
@@ -1,5 +1,6 @@
 package ru.nsu.primakova;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Stack;
@@ -9,14 +10,22 @@ import java.util.Stack;
  */
 public class IteratorDfs<T> implements Iterator<T> {
     private final Stack<Tree<T>> stack;
+    private final int length;
+    private final Tree<T> startTree;
+
 
     public IteratorDfs(Tree<T> tree) {
         this.stack = new Stack<>();
         stack.push(tree);
+        this.length = tree.get_length();
+        this.startTree = tree;
     }
 
     @Override
     public boolean hasNext() {
+        if (this.length != startTree.get_length()) {
+            throw new ConcurrentModificationException();
+        }
         return !this.stack.isEmpty();
     }
 
