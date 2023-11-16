660f8a1fe7458a09cb0c7e3af8671d11155ec654
Yulia Primakova <y.primakova@g.nsu.ru>
2023-11-16 12:29:59 +0700

    Task_1_3_1

--- Task_1_1_1/src/main/sth.bat
+++
@@ -1,5 +0,0 @@
-javac -d .\out .\java\ru\nsu\primakova\Main.java .\java\ru\nsu\primakova\Heapsort.java
-javadoc -d .\javadoc .\java\ru\nsu\primakova\Main.java .\java\ru\nsu\primakova\Heapsort.java
-cd .\out
-java -classpath . ru.nsu.primakova.Main
-pause
