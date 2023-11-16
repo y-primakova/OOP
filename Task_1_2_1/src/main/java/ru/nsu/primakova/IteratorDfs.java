2c08cc7b986884475230718e94c65e62416b11be
Yulia Primakova <y.primakova@g.nsu.ru>
2023-11-16 16:44:42 +0700

    Revert "Task_1_2_2"
    This reverts commit 188050f4302561541d34df68bb24487606202a70.

---
+++ Task_1_2_1/src/main/java/ru/nsu/primakova/IteratorDfs.java
@@ -0,0 +1,41 @@
+package ru.nsu.primakova;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+/**
+ * Iterator class using the DFS algorithm.
+ */
+public class IteratorDfs<T> implements Iterator<T> {
+    private final Stack<Tree<T>> stack;
+    private final int modification;
+    private final Tree<T> startTree;
+
+
+    public IteratorDfs(Tree<T> tree) {
+        this.stack = new Stack<>();
+        stack.push(tree);
+        this.modification = tree.get_nModification();
+        this.startTree = tree;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (this.modification != startTree.get_nModification()) {
+            throw new ConcurrentModificationException();
+        }
+        return !this.stack.isEmpty();
+    }
+
+    @Override
+    public T next() {
+        if (hasNext()) {
+            Tree<T> next = this.stack.pop();
+            this.stack.addAll(next.get_children());
+            return next.get_value();
+        }
+        throw new NoSuchElementException();
+    }
+}
