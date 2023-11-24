343e861e98272b6275f57dd60a6e697ac18b9e0d
y-primakova <y-primakova@users.noreply.github.com>
2023-11-24 06:28:16 +0000

    Deploying to gh-pages from @ y-primakova/OOP@dee49f91a9eec1012287a20543a519ac28c45901 🚀

---
+++ Task_1_5_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.primakova","l":"Calculator"},{"p":"ru.nsu.primakova","l":"IncorrectInputException"},{"p":"ru.nsu.primakova","l":"Main"}]
