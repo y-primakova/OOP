61d1bd98464b09c326ec056111997212aea6f458
y-primakova <y-primakova@users.noreply.github.com>
2024-03-01 17:43:57 +0000

    Deploying to gh-pages from @ y-primakova/OOP@a46aee9c8146a07f71ee5148ad00a08c2f8db8b5 🚀

--- Task_2_1_1/package-search-index.js
+++ Task_2_1_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.primakova.hascompositecheck"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.primakova"},{"l":"ru.nsu.primakova.hascompositecheck"}]
